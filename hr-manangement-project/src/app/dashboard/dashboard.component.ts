effae5c49a532c2f79a2f4d7729e78decb5225fd
Diego Sebastian Rios Perez <diego.rios@yuxiglobal.com>
2018-12-10 19:42:43 -0500

    front-end

--- hr-manangement-project/src/app/dashboard/dashboard.component.ts
+++ hr-manangement-project/src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   showFiller = false;
+  localStorage = localStorage.getItem('user_logged');
 
   constructor() { }
 
